6dd70d975d99669ff5973fb06c906aef82133de2
r.yurmanov@gmail.com <r.yurmanov@gmail.com>
2020-06-10 17:51:21 +0300

    final

--- src/main/java/testrenicredit/defsteps/DefSteps.java
+++ src/main/java/testrenicredit/defsteps/DefSteps.java
@@ -1,17 +1,19 @@
 package testrenicredit.defsteps;
 
 import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import testrenicredit.steps.ContributionsPageSteps;
 import testrenicredit.steps.MainPageSteps;
 import testrenicredit.utils.AllureUtils;
 
 public class DefSteps {
-    ContributionsPageSteps contributionsPageSteps = new ContributionsPageSteps();
+    private MainPageSteps mainPageSteps = new MainPageSteps();
+    private ContributionsPageSteps contributionsPageSteps = new ContributionsPageSteps();
 
     @When("Переходим на страницу Вклады")
     public void goToContributionsPage() {
-        new MainPageSteps().goToContributionsPage();
+        mainPageSteps.goToContributionsPage();
     }
 
     @When("Выбираем валюту {string}")
@@ -30,7 +32,7 @@ public class DefSteps {
         contributionsPageSteps.chooseOptions(checkBoxName);
     }
 
-    @When("Проверяем поля результатов калькулятора вкладов")
+    @Then("Проверяем результаты рассчета калькулятора вкладов")
     public void checkFields(DataTable dataTable) {
         contributionsPageSteps.checkFields(dataTable.asMap(String.class, String.class));
     }
